93f50b26c6d79086acd05bde23374302be957c4a
rjmadeja <91998689+rjmadeja@users.noreply.github.com>
2024-02-15 21:56:18 +0800

    Update CloudFirestore.kt

--- beta_ikiosk_vr14/CloudFirestore.kt
+++ beta_ikiosk_vr14/CloudFirestore.kt
@@ -32,56 +32,5 @@ class CloudFirestore : AppCompatActivity() {
             .addOnFailureListener { e ->
                 Log.w(TAG, "Error adding document", e)
             }
-
-
-        /*val data1 = hashMapOf(
-            "name" to "San Francisco",
-            "state" to "CA",
-            "country" to "USA",
-            "capital" to false,
-            "population" to 860000,
-            "regions" to listOf("west_coast", "norcal")
-        )
-        cities.document("SF").set(data1)
-
-        val data2 = hashMapOf(
-            "name" to "Los Angeles",
-            "state" to "CA",
-            "country" to "USA",
-            "capital" to false,
-            "population" to 3900000,
-            "regions" to listOf("west_coast", "socal")
-        )
-        cities.document("LA").set(data2)
-
-        val data3 = hashMapOf(
-            "name" to "Washington D.C.",
-            "state" to null,
-            "country" to "USA",
-            "capital" to true,
-            "population" to 680000,
-            "regions" to listOf("east_coast")
-        )
-        cities.document("DC").set(data3)
-
-        val data4 = hashMapOf(
-            "name" to "Tokyo",
-            "state" to null,
-            "country" to "Japan",
-            "capital" to true,
-            "population" to 9000000,
-            "regions" to listOf("kanto", "honshu")
-        )
-        cities.document("TOK").set(data4)
-
-        val data5 = hashMapOf(
-            "name" to "Beijing",
-            "state" to null,
-            "country" to "China",
-            "capital" to true,
-            "population" to 21500000,
-            "regions" to listOf("jingjinji", "hebei")
-        )
-        cities.document("BJ").set(data5)*/
     }
 }
